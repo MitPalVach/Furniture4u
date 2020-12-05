20d1620d9ea155fcdd7e1671da067ddc44ccc466
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2020-12-05 22:10:59 +0300

    how it work v.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,57 @@
-let stepblock1 = document.getElementById('stepblock1');
-let stepbrdr1 = document.getElementById('stepbrdr1');
-
-let stepblock2 = document.getElementById('stepblock2');
-let stepbrdr2 = document.getElementById('stepbrdr2');
-
-let stepblock3 = document.getElementById('stepblock3');
-let stepbrdr3 = document.getElementById('stepbrdr3');
-
-
-stepblock1.onmouseover = function () {
-    stepbrdr1.style. borderBottom = '3px solid white';
-}
-stepblock1.onmouseout = function () {
-    stepbrdr1.style. borderBottom = '3px solid black';
-}
-
-
-stepblock2.onmouseover = function () {
-    stepbrdr2.style. borderBottom = '3px solid white';
-}
-stepblock2.onmouseout = function () {
-    stepbrdr2.style. borderBottom = '3px solid black';
-}
-
-
-stepblock3.onmouseover = function () {
-    stepbrdr3.style. borderBottom = '3px solid white';
-}
-stepblock3.onmouseout = function () {
-    stepbrdr3.style. borderBottom = '3px solid black';
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
+//
+// let step_block_1 = document.getElementById('step_block_1');
+// let step_border_1 = document.getElementById('step_border_1');
+//
+// let step_block_2 = document.getElementById('step_block_2');
+// let step_border_2 = document.getElementById('step_border_2');
+//
+// let step_block_3 = document.getElementById('step_block_3');
+// let step_border_3 = document.getElementById('step_border_3');
+//
+//
+// step_block_1.onmouseover = function () {
+//     step_border_1.style.borderBottom = '3px solid white';
+// }
+// step_block_1.onmouseout = function () {
+//     step_border_1.style.borderBottom = '3px solid black';
+// }
+//
+//
+// step_block_2.onmouseover = function () {
+//     step_border_2.style.borderBottom = '3px solid white';
+// }
+// step_block_2.onmouseout = function () {
+//     step_border_2.style.borderBottom = '3px solid black';
+// }
+//
+//
+// step_block_3.onmouseover = function () {
+//     step_border_3.style.borderBottom = '3px solid white';
+// }
+// step_block_3.onmouseout = function () {
+//     step_border_3.style.borderBottom = '3px solid black';
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
